669b81a50d199deaff0e5ff1870a6b02cf3f50e4
Arbind Kumar Shahh <113020340+ArbindKumarShah@users.noreply.github.com>
2022-11-14 16:53:11 +0530

    Add files via upload

---
+++ Chatbot-Using-Html, Css and Javascript/script.js
@@ -0,0 +1,17 @@
+function talk(){
+    var know = {
+      "Hi" : "Hello, Developers Community Here.",
+      "How are you" : "Good :)",
+      "What can i do for you" : "Please Give us A Subscribe.",
+      "I love you":"I have already a boyfriend",
+      "ok" : "Thank You So Much <3",
+      "Bye" : "Okay! Will meet soon. TC:).."
+    };
+    var user = document.getElementById('userBox').value;
+      document.getElementById('chatLog').innerHTML = user + "<br>";
+    if (user in know) {
+      document.getElementById('chatLog').innerHTML = know[user] + "<br>";
+    }else{
+      document.getElementById('chatLog').innerHTML = "Sorry,I didn't understand <br>";
+    }
+  }
